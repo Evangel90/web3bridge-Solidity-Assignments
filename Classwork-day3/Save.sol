eb5ab33affe70fe107064d65b4826479f5dc5baf
Evangel90 <oladipoevangel90@gmail.com>
2026-02-18 20:46:59 +0100

    update

--- Classwork-day3/Save.sol
+++ Classwork-day3/Save.sol
@@ -13,23 +13,30 @@ contract Save {
     event DepositERC20(address indexed user, uint amount, string assetType);
     event WithdrawalEther(address indexed user, uint amount, string assetType);
     event WithdrawalERC20(address indexed user, uint amount, string assetType);
+    event AllowanceApproved(address indexed user, uint amount);
 
     function depositEther() public payable {
         require(msg.value > 0, "Deposit amount must be greater than zero");
-        etherBalances[msg.sender] += msg.value;
+        etherBalances[msg.sender] = etherBalances[msg.sender] +msg.value;
         emit DepositEther(msg.sender, msg.value, "ETH");
+    }
+
+    function allowanceApproveERC20(uint amount) public {
+        token.approve(address(this), amount);
+        emit AllowanceApproved(msg.sender, amount);
     }
 
     function depositERC20(uint amount) public {
         require(token.balanceOf(msg.sender) >= amount, "Insufficient balance");
+        require(token.allowance(msg.sender, address(this)) >= amount, "Allowance not sufficient");
         token.transferFrom(msg.sender, address(this), amount);
-        erc20Balances[msg.sender] += amount;
+        erc20Balances[msg.sender] = erc20Balances[msg.sender] + amount;
         emit DepositERC20(msg.sender, amount, "ERC20");
     }
 
     function withdrawEther(uint amount) public {
         require(etherBalances[msg.sender] >= amount, "Insufficient balance");
-        etherBalances[msg.sender] -= amount;
+        etherBalances[msg.sender] = etherBalances[msg.sender] - amount;
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Ether transfer failed");
         emit WithdrawalEther(msg.sender, amount, "ETH");
@@ -37,7 +44,8 @@ contract Save {
 
     function withdrawERC20(uint amount) public {
         require(erc20Balances[msg.sender] >= amount, "Insufficient balance");
-        erc20Balances[msg.sender] -= amount;
+        erc20Balances[msg.sender] = erc20Balances[msg.sender] - amount;
+        token.transfer(msg.sender, amount);
         emit WithdrawalERC20(msg.sender, amount, "ERC20");
     }
 
